5e1db2a36b2677f2db503c4eb7cf999e5a26c6b1
Andrii Skokovskyi <askokovskiy@gmail.com>
2022-09-26 22:35:45 +0300

    feat: Add create button and sort markup

--- src/pages/MainView/MainView.js
+++ src/pages/MainView/MainView.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import styles from "./MainView.module.css";
+
 import SearchForm from "../../components/SearchForm/SearchForm";
 import ProductsList from "../../components/Products/ProductsList";
 import Pagination from "../../components/Pagination/Pagination";
@@ -8,6 +10,19 @@ const MainView = () => {
   return (
     <>
       <SearchForm />
+      <section className={styles["content-main"]}>
+        <div className={styles["content-header"]}>
+          <h2 className="content-title">Products</h2>
+          <div>
+            <div className={styles.button}>Create</div>
+          </div>
+          <select className={styles.select}>
+            <option>Sort ⬆ </option>
+            <option>Sort ⬇ </option>
+          </select>
+        </div>
+      </section>
+
       <ProductsList />
       <Pagination />
     </>
