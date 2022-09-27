6525a555b83514ce039304dbace1409ae48228eb
Andrii Skokovskyi <askokovskiy@gmail.com>
2022-09-27 14:20:56 +0300

    feat: Add fetch data func to get products

--- src/components/Products/ProductsList.js
+++ src/components/Products/ProductsList.js
@@ -1,54 +1,22 @@
-import React from "react";
-// import Card from "../UI/Cards";
-import ProductItem from "./ProductItem";
+import React, { useEffect, useState } from "react";
 
 import styles from "./ProductsList.module.css";
 
-const DUMMY_DATA = [
-  {
-    id: "bix27468fjc",
-    title: "Audi",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, qui?",
-    price: 100,
-    inCart: true,
-  },
-  {
-    id: "BMW",
-    title: "iphone 5c",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, qui?",
-    price: 150,
-    inCart: false,
-  },
-  {
-    id: "ox98i1f5idg",
-    title: "Citroen",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, qui?",
-    price: 200,
-    inCart: false,
-  },
-  {
-    id: "adv62bxeq9i",
-    title: "Lexus",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, qui?",
-    price: 250,
-    inCart: false,
-  },
-  {
-    id: "yp5hqdyqh9b",
-    title: "Mazda",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, qui?",
-    price: 300,
-    inCart: false,
-  },
-];
+import ProductItem from "./ProductItem";
 
 const ProductsList = () => {
-  const productsList = DUMMY_DATA.map((product) => (
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch("http://localhost:3001/products");
+      const json = await data.json();
+      setProducts(json);
+    };
+    fetchData().catch(console.error);
+  }, []);
+
+  const productsList = products.map((product) => (
     <ProductItem
       key={product.id}
       title={product.title}
