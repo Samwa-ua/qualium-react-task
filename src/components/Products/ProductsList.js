e79574721e51642b86947b1dd997a5256d88be8f
Andrii Skokovskyi <askokovskiy@gmail.com>
2022-09-27 14:36:19 +0300

    feat: Add spinner while fetching data

--- src/components/Products/ProductsList.js
+++ src/components/Products/ProductsList.js
@@ -3,15 +3,18 @@ import React, { useEffect, useState } from "react";
 import styles from "./ProductsList.module.css";
 
 import ProductItem from "./ProductItem";
+import LoadingSpinner from "../UI/LoadingSpinner";
 
 const ProductsList = () => {
   const [products, setProducts] = useState([]);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetch("http://localhost:3001/products");
       const json = await data.json();
       setProducts(json);
+      setIsPending(false);
     };
     fetchData().catch(console.error);
   }, []);
@@ -24,7 +27,12 @@ const ProductsList = () => {
       description={product.description}
     ></ProductItem>
   ));
-  return <section className={styles.products}>{productsList}</section>;
+  return (
+    <section className={styles.products}>
+      {isPending && <LoadingSpinner />}
+      {productsList}
+    </section>
+  );
 };
 
 export default ProductsList;
