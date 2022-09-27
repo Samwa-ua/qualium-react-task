b201c9fced43f1e88f5303524b3aeacc42c6a003
Andrii Skokovskyi <askokovskiy@gmail.com>
2022-09-27 12:58:48 +0300

    fix: Misspell component name

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Card from "../UI/Cards";
 
 import styles from "./ProductItem.module.css";
+
+import Card from "../UI/Card";
 
 const ProductItem = (props) => {
   return (
